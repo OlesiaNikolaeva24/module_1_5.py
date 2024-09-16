26ab02d1fd5939835890d3c99d30b02665e15fc8
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-16 20:47:46 +0900

    13/09 15/09 16/09 16.09 1 16/09 2

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Olesia'
 print('Name:', name)
 age = 24
 print('Age:', age)
-age += 0.1
+age += 0.2   # age = age + 0.2, age = 24 + 0.2
 print('New age:', age)
 is_student = True
 print('Is student:', is_student)
