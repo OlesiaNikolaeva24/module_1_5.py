10dc894e109e6eaba86ef18e5bff3960aba79b15
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-16 20:12:11 +0900

    13/09 15/09 16/09

---
+++ python_homework2.py
@@ -0,0 +1,6 @@
+all_homework = 12    # Snake case
+total_time = 1.5    # Snake case
+nameCourse = 'Python'    # Camel case
+homework_time = total_time / all_homework    # Snake case
+
+print('Курс:',nameCourse + ', всего задач:', str(all_homework) + ', затрачено часов:',str(total_time) + ', среднее время выполнения',homework_time, 'часа.')
