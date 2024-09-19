012ea0970db16862bfbf94beec0967e5c573a4f2
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-19 22:38:54 +0900

    13/09 15/09 16/09 16.09 1 16/09 2 17/09 19/09 19/09

--- module_1_5.py
+++ module_1_5.py
@@ -1,16 +1,23 @@
 # Кортеж - это неизменяемая упорядоченная коллекция, которая может содержать изменяемые элементы
 immutable_var = (1, 2)
-print(immutable_var)
-immutable_var = (1, 2) * 3 + (False, 1, 2, [3, 4.5])
+print("Исходный кортеж: ", immutable_var)
+immutable_var = (1, 2) * 3 + (False, -1, 2, [3, 4.5])
 immutable_var[9][1] = ['Year']
-print(immutable_var)
+print("Обновленный кортеж: ", immutable_var)
 print()
 
 # Список - это изменяемая упорядоченная коллекция
-mutable_list = [ 1, 2.5, False, [3, 4.7], 'False', (6, 7)]
-print(mutable_list)
+mutable_list = [ 1, 2.5, False, [-3, 4.7], 'False', (6, 7)]
+print("Исходный список: ", mutable_list)
 mutable_list.extend("smile")
 mutable_list[3] = tuple(mutable_list[3]) * 3
 mutable_list[5] = list(mutable_list[5])
-mutable_list[4] = []
-print(mutable_list)
+a = mutable_list.pop(2)
+mutable_list.remove(2.5)
+mutable_list.append(a)
+print("Обновленный список: ", mutable_list)
+print()
+
+# Кортеж экономит память
+print("Вес кортежа:", immutable_var.__sizeof__(), "             Вес списка:", mutable_list.__sizeof__())
+print("Вес списка из кортежа:", list(immutable_var).__sizeof__(), "   Вес кортежа из списка:", tuple(mutable_list).__sizeof__())
