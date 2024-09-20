9f643682433a44763a36054c743db88eef99a267
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-20 16:06:01 +0900

    13/09 15/09 16/09 16.09 1 16/09 2 17/09 19/09 19/09 20/09

--- module_1_6.py
+++ module_1_6.py
@@ -3,20 +3,16 @@ my_dict = {'Olesia': 2000, 'Alisa': 2001, 'Leta': 2002}
 print('Dictionary:', my_dict)
 print('Existing value:', my_dict.get("Olesia"))
 print('Not existing value:', my_dict.get("?"))
-my_dict["Danit"] = 'XXXX'
-print('Modified dictionary:', my_dict)
-
 my_dict.update({'Artur': '19XX', 'Sense': '19XX'})
-print(my_dict)
-
-a = my_dict.pop('Olesia')
-print(a)
-print(my_dict)
-
+a = my_dict.pop('Alisa')
+print('Deleted value:', a)
+print('Modified dictionary:', my_dict)
+print()
 # Работа с множествами
 my_set = {1, 2, 2, 2, 13, 13, 44, True, (1, 2)}
 print('Set:', my_set)
 my_set.add('Terra')
 my_set.add('***')
-my_set.discard(2)
+my_set.update('Terra', '***')
+my_set.discard((1,2))
 print('Modified set:', my_set)
