39adc500386faf0c3054cd350203a77b9a7f7cf4
OlesiaNikolaeva24 <kobaltbrom@gmail.com>
2024-09-21 16:43:02 +0900

    13/09 15/09 16/09 16.09 1 16/09 2 17/09 19/09 19/09 20/09 21/09 21/09 1

--- Доп задание.py
+++ Доп задание.py
@@ -1,5 +1,6 @@
 # Способ 1
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+for i in range(len(grades)): grades[i] = sum(grades[i]) / len(grades[i])
 students = sorted(list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}))
 print(dict(zip(students, grades)))
 
@@ -9,7 +10,9 @@ students = sorted(list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}))
 s0 = []
 for i in range(len(students)):
     s = []
+    grades[i] = sum(grades[i]) / len(grades[i])
     s.append(students[i])
     s.append(grades[i])
     s0.append(s)
 print(dict(s0))
+
